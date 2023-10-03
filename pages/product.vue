b2e33cdb8ff285a2585528175da93d20ec21f3a6
susu3131 <anny81327@gmail.com>
2023-10-03 11:48:26 +0800

    feat : 動態路由

--- pages/product.vue
+++ pages/product.vue
@@ -1,8 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const id = 123;
+const id2 = 456;
+</script>
 
 <template>
   <div>
     <p>product</p>
+    <nuxt-link :to="`/product/${id}`" style="margin-right: 8px">商品細節1</nuxt-link>
+    <nuxt-link :to="`/product/${id2}`">商品細節2</nuxt-link>
+    <NuxtPage />
   </div>
 </template>
 
---
+++ pages/product/[id].vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <p>id: {{ $route.params.id }}</p>
+  </div>
+</template>
+
+<style scoped></style>
