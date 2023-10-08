71935c3fda2c4d1cbb3426e98c9dba18f1ac2334
su <anny81327@gmail.com>
2023-10-08 11:46:34 +0800

    feat : 新增api

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
+    
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <div style="background: #ffcaca; padding: 10px; width: 200px">
+      {{ test }}
+    </div>
   </div>
 </template>
+
+<script setup>
+const test = await $fetch("/api/test");
+</script>
